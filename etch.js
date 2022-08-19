4478b89a1aa0f56769b6ae61e2590f5f26d65dac
Aleksander Kowalski <108731948+volcanicviper@users.noreply.github.com>
2022-08-19 14:29:28 +0200

    Made the colors on hover random

--- etch.js
+++ etch.js
@@ -5,13 +5,15 @@ for (var i = 0; i < 256; ++i) {
      div = document.createElement('div');
     fragment.appendChild(div);
 }
-
 container.appendChild(fragment);
+const cells = container.querySelectorAll("div");
 
-const cells = container.querySelectorAll(":scope > div");
+function randomColor() {
+    return "#" + Math.floor(Math.random()*16777215).toString(16);
+}
 
 cells.forEach((item) => {
     item.addEventListener("mouseover", (event) => { 
-        event.target.style.backgroundColor = "orange";
+        event.target.style.backgroundColor = randomColor();
     });
 });
