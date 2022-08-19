23d4fcaa03ea05ca645a047d517a05b68e290cbc
Aleksander Kowalski <108731948+volcanicviper@users.noreply.github.com>
2022-08-19 13:44:49 +0200

    Made the grid

--- etch.js
+++ etch.js
@@ -1,11 +1,9 @@
 const container = document.getElementById('container');
 let fragment = document.createDocumentFragment();
 
-for (var i = 0; i < 16; ++i) {
-    for (var j = 0; j < 16; ++j) {
-        div = document.createElement('div');
-        fragment.appendChild(div);
-    }
+for (var i = 0; i < 256; ++i) {
+     div = document.createElement('div');
+    fragment.appendChild(div);
 }
 
 container.appendChild(fragment);
